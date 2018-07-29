d5d879fcb35b95b3d105c6ee01e3439d5c23da2f
Trey Hoover <treyhoover@gmail.com>
2018-07-28 23:17:28 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const {
+  URL,
+  INTERVAL,
+  HTML_PATH,
+  SLACK_TOKEN,
+  SLACK_CONVERSATION_ID
+} = require("dotenv").config().parsed;
+const sleep = require("sleep-promise");
+const axios = require("axios");
+const { RTMClient } = require("@slack/client");
+
+const slack = new RTMClient(SLACK_TOKEN);
+slack.start();
+
+const notifyGeneral = msg =>
+  slack.sendMessage(`<!channel>: ${msg}`, SLACK_CONVERSATION_ID);
+
+// Main loop
+async function main() {
+  // Initialize baseline html file if it doesn't exist
+  if (!fs.existsSync(HTML_PATH)) {
+    console.log("Fetching baseline html");
+
+    const { data: html } = await axios.get(URL);
+
+    fs.writeFileSync(HTML_PATH, html);
+  }
+
+  const prevHtml = fs.readFileSync(HTML_PATH, "utf-8");
+
+  while (true) {
+    await sleep(parseInt(INTERVAL, 10));
+
+    console.log("Checking...");
+
+    try {
+      const { data: html } = await axios.get(URL);
+
+      if (html !== prevHtml) {
+        // updating base html
+        fs.writeFileSync(HTML_PATH, html);
+
+        const msg = await notifyGeneral(`The page has changed! (${URL})`);
+
+        console.log("Notification sent", msg.ts);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+main();
